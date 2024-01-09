dffa46aa09da3cf0539c1b46ac5ba8ce7423d675
bemadeveloper <akbermet1990@icloud.com>
2024-01-09 14:35:58 +0600

    Clean comments

--- CreatingAppForChangingColor/ViewController.swift
+++ CreatingAppForChangingColor/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  CreatingAppForChangingColor
-//
-//  Created by Bema on 9/1/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func setupView() {
